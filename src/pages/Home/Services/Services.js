5f48179db2c5714fe3c8264fafe7ab25bc6c0c25
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-12 11:41:06 +0600

    Responsive Grid for services section

---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import fluoride from '../../../assets/images/fluoride.png'
+import cavity from '../../../assets/images/cavity.png'
+import whitening from '../../../assets/images/whitening.png'
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+
+
+    const serviceData = [
+        {
+            id: 1,
+            name: 'Fluoride Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            icon: fluoride,
+            bgClass: 'bg-gradient-to-r from-primary to-secondary'
+        },
+        {
+            id: 2,
+            name: 'Cavity Filling',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            icon: cavity,
+            bgClass: 'bg-gradient-to-r from-primary to-secondary'
+        },
+        {
+            id: 3,
+            name: 'Teeth Whitening',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            icon: whitening,
+            bgClass: 'bg-gradient-to-r from-primary to-secondary'
+        },
+    ]
+
+
+    return (
+        <div className='my-24'>
+            <div>
+                <h4 className='text-center text-base text-primary'>OUR SERVICES</h4>
+                <h4 className='text-3xl text-center text-base text-black'>Services We Provide</h4>
+            </div>
+            <div className='grid grid-cols-1 md:grid-rows-2 lg:grid-cols-3 mt-4 gap-6'>
+                {
+                    serviceData.map(service => <ServiceCard
+                        key={service.id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
