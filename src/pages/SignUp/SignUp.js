052ff4ca73b2e3a3c56bc020fa3afdf07c740d42
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-14 00:28:01 +0600

    Use RegEx validation in React Hook form

---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handleSignUp = data => {
+        console.log(data)
+    }
+
+
+    return (
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input {...register("name", { required: "Name Address is required" })} type="text" className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500' role="alert">{errors.name?.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("email", { required: "Email Address is required" })} type="text" className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500' role="alert">{errors.email?.message}</p>}
+                    </div>
+
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input {...register("password", {
+                            required: "Password Address is required", minLength: { value: 6, message: 'Passwor must be 6 characters or longer' }, pattern: {
+                                value: /(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]/, message: "Entered value does not match password format"
+                            }
+                        })} type="password" className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500' role="alert">{errors.password?.message}</p>}
+                        <label className="label">
+                            <span className="label-text">Forgot Password ?</span>
+                        </label>
+                    </div>
+
+
+                    <input className='btn btn-accent w-full text-white' value="Sign Up" type="submit" />
+                    <p>Already have an Account <Link className='text-secondary' to="/login">Please Login</Link></p>
+                    <div className="divider">OR</div>
+                    <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
