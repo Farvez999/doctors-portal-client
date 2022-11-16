e448e578283f7bf263de91de4f3a2ac073638fe0
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-17 03:25:21 +0600

    Display User specific Appointments using Data Table

--- src/Dashboard/Dashboard/MyAppoinment.js
+++ src/Dashboard/Dashboard/MyAppoinment.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import { async } from '@firebase/util';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const MyAppoinment = () => {
+
+    const { user } = useContext(AuthContext);
+
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
+    const { data: bookings = [] } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        }
+    })
     return (
         <div>
             <h3 className='text-3xl mb-5'>My Appoinment</h3>
@@ -12,32 +28,22 @@ const MyAppoinment = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Treatment</th>
+                            <th>Date</th>
+                            <th>Time</th>
                         </tr>
                     </thead>
                     <tbody>
-
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-
-                        <tr className="hover">
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+                        {
+                            bookings.map((bookings, i) =>
+                                <tr>
+                                    <th>{i}</th>
+                                    <td>Cy Ganderton</td>
+                                    <td>Quality Control Specialist</td>
+                                    <td>Blue</td>
+                                    <td>Blue</td>
+                                </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
